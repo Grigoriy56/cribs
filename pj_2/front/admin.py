16c0ddc1419cfa80107f05e39c80d3c8df391b21
Grisha <grisha9hs@gmail.com>
2020-12-08 23:10:11 +0300

    plz,God,do_button

--- pj_2/front/admin.py
+++ pj_2/front/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Thems
 
-# Register your models here.
+admin.site.register(Thems)
--- pj_2/front/views.py
+++ pj_2/front/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Thems
 
 
 def index(request):
-    return render(request, 'front/index.html')
+    results = Thems.objects.all()
+    return render(request, 'front/index.html', {'thems': results})
 
 
 def about(request):
@@ -11,3 +13,23 @@ def about(request):
 
 def algem(request):
     return render(request, 'front/algem.html')
+
+
+def matrix(request):
+    return render(request, 'front/matrix.html')
+
+
+def matrix_a(request):
+    return render(request, 'front/matrix_addition.html')
+
+
+def matrix_mult(request):
+    return render(request, 'front/matrix_multiplication.html')
+
+
+def matrix_mult1(request):
+    return render(request, 'front/matrix_multiplication_na_chyslo.html')
+
+
+def matrix_tr(request):
+    return render(request, 'front/matrix_transpose.html')
