aa49131f5922379160192ae7225dcce9a8baf5f1
Grisha <grisha9hs@gmail.com>
2020-12-10 09:50:36 +0300

    delete_key

--- pj_2/pj_2/settings.py
+++ pj_2/pj_2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q63^n3^zf^9j4xah%wevk^fkkvbxkskv0er)e3@)a&4@vo!1mx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
